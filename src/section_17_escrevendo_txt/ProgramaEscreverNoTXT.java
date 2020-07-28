eae90d72c0549ce75a29aa6fc9660bb21fc68a51
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2020-07-28 00:54:41 -0300

    Cont aula 184 Manipular pastas com file

---
+++ src/section_17_escrevendo_txt/ProgramaEscreverNoTXT.java
@@ -0,0 +1,23 @@
+package section_17_escrevendo_txt;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProgramaEscreverNoTXT {
+    public static void main(String[] args) {
+        String[] listaHerois = new String[]{"Capitã Marvel", "Spider-Man", "Feiticeira"};
+
+        String pathHeroi = "src\\section_17_escrevendo_txt\\herois.txt";
+
+        // parametro TRUE indica que não queremo recriar o arquivo e sim adicionar mais informações
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(pathHeroi, true))) {
+            for (String linha : listaHerois) {
+                bufferedWriter.write(linha);
+                bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
