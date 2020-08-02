227673580e8ab31b4a0bdcaef87f643054416379
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2020-08-01 22:33:57 -0300

    Cont aula 184 Manipular pastas com file

--- src/section_17_exercicio_fixacao/ProdutoApplication.java
+++ src/section_17_exercicio_fixacao/ProdutoApplication.java
@@ -1,4 +1,52 @@
 package section_17_exercicio_fixacao;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ProdutoApplication {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o caminho do arquivo: ");
+        // Caminho do arquivo: src/section_17_exercicio_fixacao/arquivos/Produtos.csv
+        String caminho = scanner.nextLine();
+
+        File file = new File(caminho);
+        final boolean mkdir = new File(file.getParent() + "\\out").mkdir();
+        String arquivoAlvo = file.getParent() + "\\out\\resumo.csv";
+
+        List<ProdutoEntity> listaProdutos = new ArrayList<>();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(caminho))) {
+
+            String linhaCsv = bufferedReader.readLine();
+
+            while (linhaCsv != null) {
+
+                String[] campos = linhaCsv.split(";");
+                String nome = campos[0];
+                Double preco = Double.parseDouble(campos[1]);
+                Integer quantidade = Integer.parseInt(campos[2]);
+
+                listaProdutos.add(new ProdutoEntity(nome, preco, quantidade));
+
+                linhaCsv = bufferedReader.readLine();
+            }
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(arquivoAlvo))) {
+                for (ProdutoEntity produtoEntity : listaProdutos) {
+                    bufferedWriter.write(produtoEntity.getNome() + "; " + produtoEntity.total());
+                    bufferedWriter.newLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Erro ao escrever novo arquivo " + e.getMessage());
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao ler arquivo " + e.getMessage());
+        }
+
+        scanner.close();
+    }
 }
--- src/section_17_exercicio_fixacao/ProdutoEntity.java
+++ src/section_17_exercicio_fixacao/ProdutoEntity.java
@@ -1,4 +1,51 @@
 package section_17_exercicio_fixacao;
 
 public class ProdutoEntity {
+
+    private String nome;
+    private Double valor;
+    private Integer quantidade;
+
+    public ProdutoEntity(String nome, Double valor, Integer quantidade) {
+        this.nome = nome;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double total() {
+        return getValor() * getQuantidade();
+    }
+
+    @Override
+    public String toString() {
+        return "ProdutoEntity{" +
+                "nome='" + nome + '\'' +
+                ", valor=" + valor +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
